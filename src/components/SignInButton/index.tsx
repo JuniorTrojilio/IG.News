e585c2dfdbe702410b5f4e4e468150bbe57edd30
Junior Trojilio <osmar.trojilio@gmail.com>
2021-08-04 17:01:40 -0300

    feat: add Next Provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button className={styles.signinbutton} type="button">
       <FaGithub color="#04d361"/>
-      Junior Trojilio
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()}/>
     </button>
   ) : (
-    <button className={styles.signinbutton} type="button">
+    <button className={styles.signinbutton} type="button" onClick={() => signIn('github')}>
       <FaGithub color="#eba417"/>
       Sign in with Github
     </button>
